18f2eb83cd5ca48f87a5a6e1ab9b95463ada95fb
TakayukiKiyohara <nezumimusume30@gmail.com>
2017-04-03 00:47:09 +0900

    ConstantBufferを用意。

---
+++ DirectXⅣ/Tutorial01/tkEngine2/graphics/GPUPipelineBuffer/tkConstantBuffer.cpp
@@ -0,0 +1,16 @@
+/*!
+ * @brief	定数バッファ。
+ */
+
+#include "tkEngine2/tkEnginePreCompile.h"
+#include "tkEngine2/graphics/GPUPipelineBuffer/tkConstantBuffer.h"
+
+
+namespace tkEngine2{
+	CConstantBuffer::CConstantBuffer()
+	{
+	}
+	CConstantBuffer::~CConstantBuffer()
+	{
+	}
+}
---
+++ DirectXⅣ/Tutorial01/tkEngine2/graphics/GPUPipelineBuffer/tkConstantBuffer.h
@@ -0,0 +1,16 @@
+/*!
+ * @brief	定数バッファ
+ */
+
+#pragma once
+
+namespace tkEngine2{
+	/*!
+	 * @brief	定数バッファ。
+	 */
+	class CConstantBuffer{
+	public:
+		CConstantBuffer();
+		~CConstantBuffer();
+	};
+}
